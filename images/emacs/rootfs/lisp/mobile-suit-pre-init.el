51b37373b4f73f342bd44168fc8d929a859aaa5b
Johnson Denen <johnson.denen@gmail.com>
2017-11-28 15:54:37 -0500

    Copy private SSH keys to $HOME/.ssh

--- images/emacs/rootfs/lisp/mobile-suit-pre-init.el
+++ images/emacs/rootfs/lisp/mobile-suit-pre-init.el
@@ -1,3 +1,6 @@
 ;;; mobile-suit-pre-init.el --- executed before `mobile-suit/-init'
-
+(dolist  (file (directory-files "/tmp/private/.ssh" nil "^[^\.]"))
+  (let ((source (format "/tmp/private/.ssh/%s" file))
+        (destination (format "/home/emacs/.ssh/%s" file)))
+    (copy-file source destination t)))
 ;;; mobile-suit-pre-init.el ends here
