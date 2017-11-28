95c84304778cff423a08c98a52afaceafc9b637f
Johnson Denen <johnson.denen@gmail.com>
2017-11-27 23:25:44 -0500

    Minor scripting tweaks

--- images/emacs/rootfs/lisp/mobile-suit.el
+++ images/emacs/rootfs/lisp/mobile-suit.el
@@ -7,34 +7,21 @@
 ;;; Code:
 (require 'tramp)
 
-(defconst mobile-suit/images '(groovy)
+(defvar mobile-suit/images '()
   "Images spun up with Emacs.")
 
-(defvar mobile-suit/master-key "/home/emacs/key.asc"
+(defvar mobile-suit/master-key "/tmp/private/key.asc"
   "Secret key location. Will be deleted after it's imported.")
 
-(defun mobile-suit/import-master-key ()
+(defun mobile-suit/-import-master-key ()
   "Imports and deletes GnuPG private key located: `mobile-suit/master-key'."
   (let ((key `,mobile-suit/master-key))
-    (epa-import-keys key)
     (when (file-exists-p key)
+      (epa-import-keys key)
       (delete-file key))))
 
-(defun mobile-suit/decrypt-ssh-keys ()
-  "Decrypts GnuPG encrypted .ssh keys."
-  (message "[mobile-suite-emacs] decrypting SSH keys...")
-  (let* ((encrypted (directory-files "/home/emacs/.ssh" t "^.*\.gpg$"))
-         (decrypted (let ((result ()))
-                      (dolist (key encrypted)
-                        (push (replace-regexp-in-string "\.gpg$" "_rsa" key) result))
-                      (reverse result)))
-         (zipped (cl-mapcar #'cons encrypted decrypted)))
-    (dolist (keypair zipped)
-      (epa-decrypt-file (car keypair) (cdr keypair))))
-  (message "[mobile-suite-emacs] ...done"))
-
-(defun mobile-suit/copy-pub-key-to-image ()
-  "Copies SSH pub key needed for `tramp' to `mobile-suite/images'."
+(defun mobile-suit/-copy-pub-key-to-images ()
+  "Copy SSH pub key needed for `tramp' to `mobile-suite/images'."
   (message "[mobile-suit] copying SSH pub key to images...")
   (let ((passfile "/home/emacs/.password"))
     (with-temp-file passfile
@@ -43,11 +30,11 @@
       (shell-command (format "sshpass -f %s ssh-copy-id %s"
                              passfile image)))
     (delete-file passfile))
-  (message "[mobile-suite-emacs] ...done"))
+  (message "[mobile-suit] ...done"))
 
-(defun mobile-suit/set-tramp-properties ()
+(defun mobile-suit/-set-tramp-properties ()
   "Set remote-shell `tramp-connection-properties' for `mobile-suit/images'."
-  (message "[mobile-suit] settings shell to /bin/bash on images...")
+  (message "[mobile-suit] setting TRAMP properties...")
   (dolist (image `,mobile-suit/images)
     (dolist (keypair `(("remote-shell" . "/bin/bash")))
       (add-to-list 'tramp-connection-properties
@@ -56,15 +43,24 @@
                          (cdr keypair)))))
   (message "[mobile-suit] ...done"))
 
-(defun mobile-suit/copy-ssh-keys-to-image ()
-  "Copies SSH private keys to `mobile-suit/images'."
+(defun mobile-suit/-decrypt-ssh-keys ()
+  "Decrypt *.gpg encrypted files located in /home/emacs/.ssh directory.
+
+Decrypted file name truncates .gpg extension."
+  (message "[mobile-suit] decrypting SSH keys...")
+  (dolist (key (directory-files "/home/emacs/.ssh" t "\.gpg"))
+    (epa-decrypt-file key (replace-regexp-in-string "\.gpg" "" key)))
+  (message "[mobile-suit] ...done"))
+
+(defun mobile-suit/-copy-ssh-keys-to-images ()
+  "Copy SSH keys to `mobile-suit/images'."
   (message "[mobile-suit] copying SSH keys to images...")
   (dolist (image `,mobile-suit/images)
     (dolist (key (directory-files "/home/emacs/.ssh" t "[^id]_rsa$"))
       (copy-file key (format "/ssh:%s:/home/emacs/.ssh" image) t)))
   (message "[mobile-suit] ...done"))
 
-(defun mobile-suit/chown-image-code-volume ()
+(defun mobile-suit/-chown-image-code-volumes ()
   "Change ownership of /home/emacs/code on `mobile-suit/images' to the spacemacser user."
   (message "[mobile-suit] chowning /home/emacs/code on images...")
   (dolist (image `,mobile-suit/images)
@@ -76,12 +72,12 @@
   "Configure the mobile-suit environment."
   (progn
     (message "[mobile-suit] initializing...")
-    (mobile-suit/import-master-key)
-    (mobile-suit/copy-pub-key-to-image)
-    (mobile-suit/decrypt-ssh-keys)
-    (mobile-suit/copy-ssh-keys-to-image)
-    (mobile-suit/set-tramp-properties)
-    (mobile-suit/chown-image-code-volume)))
+    (mobile-suit/-import-master-key)
+    (mobile-suit/-copy-pub-key-to-images)
+    (mobile-suit/-decrypt-ssh-keys)
+    (mobile-suit/-copy-ssh-keys-to-images)
+    (mobile-suit/-set-tramp-properties)
+    (mobile-suit/-chown-image-code-volumes)))
 
 (eval-after-load 'mobile-suit
   (mobile-suit/init))
