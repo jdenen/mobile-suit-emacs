3a1fb0214d16a76d2908fb96c1306a7fdab36012
Johnson Denen <johnson.denen@gmail.com>
2017-11-29 03:06:24 +0000

    Add with-message macro

--- images/emacs/rootfs/lisp/mobile-suit.el
+++ images/emacs/rootfs/lisp/mobile-suit.el
@@ -13,6 +13,15 @@
 (defvar mobile-suit/master-key "/tmp/private/key.asc"
   "Secret key location. Will be deleted after it's imported.")
 
+(defmacro mobile-suit/with-message (msg &rest body)
+  "`message' MSG before executing BODY."
+  (declare (indent defun))
+  (let ((fmsg (format "[mobile-suit] %s..." msg)))
+    `(progn
+       (message ,fmsg)
+       ,@body
+       (message "[mobile-suit] ...done"))))
+
 (defun mobile-suit/-import-master-key ()
   "Imports and deletes GnuPG private key located: `mobile-suit/master-key'."
   (let ((key `,mobile-suit/master-key))
@@ -22,88 +31,71 @@
 
 (defun mobile-suit/-copy-pub-key-to-images ()
   "Copy SSH pub key needed for `tramp' to `mobile-suite/images'."
-  (message "[mobile-suit] copying SSH pub key to images...")
-  (let ((passfile "/home/emacs/.password"))
-    (with-temp-file passfile
-      (insert "password"))
-    (dolist (image `,mobile-suit/images)
-      (shell-command (format "sshpass -f %s ssh-copy-id %s"
-                             passfile image)))
-    (delete-file passfile))
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "copying SSH pub key to images"
+    (let ((passfile "/home/emacs/.password"))
+      (with-temp-file passfile
+        (insert "password"))
+      (dolist (image `,mobile-suit/images)
+        (shell-command (format "sshpass -f %s ssh-copy-id %s"
+                               passfile image)))
+      (delete-file passfile))))
 
 (defun mobile-suit/-set-tramp-properties ()
   "Set remote-shell `tramp-connection-properties' for `mobile-suit/images'."
-  (message "[mobile-suit] setting TRAMP properties...")
-  (dolist (image `,mobile-suit/images)
-    (dolist (keypair `(("remote-shell" . "/bin/bash")))
-      (add-to-list 'tramp-connection-properties
-                   (list (regexp-quote (format "/ssh:%s:" image))
-                         (car keypair)
-                         (cdr keypair)))))
-  (message "[mobile-suit] ...done"))
+ (mobile-suit/with-message "setting TRAMP properties"
+   (dolist (image `,mobile-suit/images)
+     (dolist (keypair `(("remote-shell" . "/bin/bash")))
+       (add-to-list 'tramp-connection-properties
+                    (list (regexp-quote (format "/ssh:%s:" image))
+                          (car keypair)
+                          (cdr keypair)))))))
 
 (defun mobile-suit/-decrypt-ssh-keys ()
   "Decrypt *.gpg encrypted files located in /home/emacs/.ssh directory.
 
 Decrypted file name truncates .gpg extension."
-  (message "[mobile-suit] decrypting SSH keys...")
-  (dolist (key (directory-files "/home/emacs/.ssh" t "\.gpg"))
-    (epa-decrypt-file key (replace-regexp-in-string "\.gpg" "" key)))
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "decrypting SSH keys"
+    (dolist (key (directory-files "/home/emacs/.ssh" t "\.gpg"))
+      (epa-decrypt-file key (replace-regexp-in-string "\.gpg" "" key)))))
 
 (defun mobile-suit/-copy-ssh-keys-to-images ()
   "Copy SSH keys to `mobile-suit/images'."
-  (message "[mobile-suit] copying SSH keys to images...")
-  (dolist (image `,mobile-suit/images)
-    (dolist (key (directory-files "/home/emacs/.ssh" t "[^id]_rsa$"))
-      (copy-file key (format "/ssh:%s:/home/emacs/.ssh" image) t)))
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "copying SSH keys to images"
+    (dolist (image `,mobile-suit/images)
+      (dolist (key (directory-files "/home/emacs/.ssh" t "[^id]_rsa$"))
+        (copy-file key (format "/ssh:%s:/home/emacs/.ssh" image) t)))))
 
 (defun mobile-suit/-chown-image-code-volumes ()
   "Change ownership of /home/emacs/code on `mobile-suit/images' to the spacemacser user."
-  (message "[mobile-suit] chowning /home/emacs/code on images...")
-  (dolist (image `,mobile-suit/images)
-    (let ((default-directory (format "/ssh:%s:/home/emacs" image)))
-      (shell-command "sudo chown spacemacser:emacs code")))
-  (message "[mobile-suit] ...done"))
-
-(defun mobile-suit/-load-custom-lisp-scripts ()
-  "Load custom script files."
-  (message "[mobile-suit] loading custom /lisp scripts...")
-  (let ((custom-scripts (remove "/lisp/mobile-suit.el"
-                                (directory-files "/lisp" t "\.el$"))))
-    (dolist (script custom-scripts)
-      (load-file script)))
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "chowning /home/emacs/code on images"
+    (dolist (image `,mobile-suit/images)
+      (let ((default-directory (format "/ssh:%s:/home/emacs" image)))
+        (shell-command "sudo chown spacemacser:emacs code")))))
 
 (defun mobile-suit/-pre-init ()
   "Load custom script before `mobile-suit/-init' execution."
-  (message "[mobile-suit] loading pre-init script...")
-  (load-file "/lisp/mobile-suit-pre-init.el")
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "loading pre-init script"
+    (load-file "/lisp/mobile-suit-pre-init.el")))
 
 (defun mobile-suit/-post-init ()
   "Load custom script after `mobile-suit/-init' execution."
-  (message "[mobile-suit] loading post-init script...")
-  (load-file "/lisp/mobile-suit-post-init.el")
-  (message "[mobile-suit] ...done"))
+  (mobile-suit/with-message "loading post-init script"
+    (load-file "/lisp/mobile-suit-post-init.el")))
 
 (defun mobile-suit/-init ()
   "Configure the mobile-suit environment."
-  (progn
-    (message "[mobile-suit] initializing...")
-    (mobile-suit/-import-master-key)
-    (mobile-suit/-copy-pub-key-to-images)
-    (mobile-suit/-decrypt-ssh-keys)
-    (mobile-suit/-copy-ssh-keys-to-images)
-    (mobile-suit/-set-tramp-properties)
-    (mobile-suit/-chown-image-code-volumes)))
+  (mobile-suit/-import-master-key)
+  (mobile-suit/-copy-pub-key-to-images)
+  (mobile-suit/-decrypt-ssh-keys)
+  (mobile-suit/-copy-ssh-keys-to-images)
+  (mobile-suit/-set-tramp-properties)
+  (mobile-suit/-chown-image-code-volumes))
 
 (eval-after-load 'mobile-suit
-  (mobile-suit/-pre-init)
-  (mobile-suit/-init)
-  (mobile-suit/-post-init))
+  (progn
+    (mobile-suit/-pre-init)
+    (mobile-suit/-init)
+    (mobile-suit/-post-init)))
 
 (provide 'mobile-suit)
 ;;; mobile-suit.el ends here
